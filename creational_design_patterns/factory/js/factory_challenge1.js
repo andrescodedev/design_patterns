2bd86b55e44f19dc559ea3f2a37c0f5e112cb8bb
andres hernandez <andrescodedev@gmail.com>
2024-11-26 12:19:21 -0500

    I finished to work the factory design pattern challenge

--- creational_design_patterns/factory/js/factory_challenge1.js
+++ creational_design_patterns/factory/js/factory_challenge1.js
@@ -2,30 +2,57 @@
     HTTP ADAPTER CHALLENGE
 */
 
-class InterfaceCar {
-    getCars(){throw new Error(`This method can't be implemented`);}
-    addCar(car){throw new Error(`This method can't be implemented`);}
-    updateCar(car){throw new Error(`This method can't be implemented`);}
-    deleteCar(carId){throw new Error(`This method can't be implemented`);}
-}
-
-class MastodonCar extends InterfaceCar {
-    static mastodonCars = [];
+//ENTITIES
+class Car {
+    static cars = [];
 
     constructor(id, name, model, price) {
-        super();
         this.id = id;
         this.name = name;
         this.model = model;
         this.price = price;
     }
+}
+
+class MastodonCar extends Car {
+    static mastodonCars = [];
+
+    constructor(id, name, model, price) {
+        super(id,name,model,price);
+    }
+}
+
+class RhinoCar extends Car {
+    static rhinoCars = [];
+
+    constructor(id, name, model, price) {
+        super(id, name, model, price);
+    }
+}
+
+
+//SERVICES
+class CarServiceInterface {
+    getCars(){throw new Error(`This method can't be implemented`);}
+    getCarsByType(carType){throw new Error(`This method can't be implemented`);}
+    addCar(car){throw new Error(`This method can't be implemented`);}
+    updateCar(car){throw new Error(`This method can't be implemented`);}
+    deleteCar(carId){throw new Error(`This method can't be implemented`);}
+}
+
+class MastodonCarService extends CarServiceInterface {
 
     getCars() {
-        return MastodonCar.mastodonCars;
+        console.log(Car.cars);
+    }
+
+    getCarsByType(carType) {
+        MastodonCar.mastodonCars = Car.cars.filter(car => car instanceof carType);
+        console.log(MastodonCar.mastodonCars);
     }
 
     addCar(car) {
-        MastodonCar.mastodonCars.push(car);
+        Car.cars.push(car);
     }
 
     updateCar(car) {
@@ -38,23 +65,14 @@ class MastodonCar extends InterfaceCar {
 
 }
 
-class RhinoCar extends InterfaceCar {
-    static rhinoCars = [];
-
-    constructor(id, name, model, price) {
-        super();
-        this.id = id;
-        this.name = name;
-        this.model = model;
-        this.price = price;
-    }
+class RhinoCarService extends CarServiceInterface {
 
     getCars() {
-        return RhinoCar.rhinoCars;
+        console.log(Car.cars);
     }
 
     addCar(car) {
-        RhinoCar.rhinoCars.push(car);
+        Car.cars.push(car);
     }
 
     updateCar(car) {
@@ -67,37 +85,133 @@ class RhinoCar extends InterfaceCar {
 
 }
 
-class InterfaceCarFactory {
-    makeCar() {
+class CarServiceInterfaceFactory {
+    makeCarService() {
         throw new Error(`The interface method can't be implemented`);
     }
 }
 
-class MastodonCarFactory extends InterfaceCarFactory {
-    makeCar(id, name, model, price) {
-        //Work constructor method without parameters.
-        return new MastodonCar(id, name, model, price);
+class MastodonCarServiceFactory extends CarServiceInterfaceFactory {
+    makeCarService() {
+        return new MastodonCarService();
     }
 }
 
-class RhinoCarFactory extends InterfaceCarFactory {
-    makeCar(id, name, model, price) {
-        return new RhinoCar(id, name, model, price);
+class RhinoCarServiceFactory extends CarServiceInterfaceFactory {
+    makeCarService() {
+        return new RhinoCarService();
     }
 }
 
 class InterfaceHttpAdapter {
-    get(carFactory) {throw new Error(`This method can't be implemented`)}
-    post(carFactory,json) {throw new Error(`This method can't be implemented`)}
-    put(carFactory) {throw new Error(`This method can't be implemented`)}
-    delete(carFactory) {throw new Error(`This method can't be implemented`)}
+    get() {throw new Error(`This method can't be implemented`)}
+    getByType(carType) {throw new Error(`This method can't be implemented`)}
+    post(json) {throw new Error(`This method can't be implemented`)}
+    put(json) {throw new Error(`This method can't be implemented`)}
+    delete(carId) {throw new Error(`This method can't be implemented`)}
 }
 
 class NestJs extends InterfaceHttpAdapter {
+    carService;
+
+    constructor(carServiceFactory) {
+        super();
+        this.carService = carServiceFactory.makeCarService();
+    }
     
-    get(carFactory, json) {
-        
+    get() {
+        this.carService.getCars();
     }
 
+    getByType(carType) {
+        this.carService.getCarsByType(carType);
+    }
+
+    post(json) {
+        this.carService.addCar(json);
+    }
+
+    put(json) {
+        this.carService.updateCar(json);
+    }
+
+    delete(carId) {
+        this.carService.deleteCar(carId);
+    }
     
 }
+
+class ExpressJs extends InterfaceHttpAdapter {
+    carService;
+
+    constructor(carServiceFactory) {
+        super();
+        this.carService = carServiceFactory.makeCarService();
+    }
+    
+    get() {
+        this.carService.getCars();
+    }
+
+    post(json) {
+        this.carService.addCar(json);
+    }
+
+    put(json) {
+        this.carService.updateCar(json);
+    }
+
+    delete(carId) {
+        this.carService.deleteCar(carId);
+    }
+}
+
+class InterfaceHttpAdapterFactory {
+    useHttpAdapter(carServiceFactory) {throw new Error(`This method can't be implemented`)}
+}
+
+class NestJsFactory extends InterfaceHttpAdapterFactory {
+    useHttpAdapter(carServiceFactory) {
+        return new NestJs(carServiceFactory);
+    }
+}
+
+class ExpressJsFactory extends InterfaceHttpAdapterFactory {
+    useHttpAdapter(carServiceFactory) {
+        return new ExpressJs(carServiceFactory);
+    }
+}
+
+function testNestJsAdapter() {
+    const car1 = new MastodonCar('1','mastodon X','2016',35000000);
+    const car2 = new MastodonCar('2','mastodon L','2020',45000000);
+    const car3 = new RhinoCar('50','Rhino L','2020',85000000);
+    const car4 = new RhinoCar('51','Rhino M','2020',85000000);
+    const car5 = new RhinoCar('52','Rhino P','2022',95000000);
+
+    const nestJsFactory = new NestJsFactory(); 
+    const nestJsAdapter1= nestJsFactory.useHttpAdapter(new MastodonCarServiceFactory());
+    const nestJsAdapter2= nestJsFactory.useHttpAdapter(new RhinoCarServiceFactory());
+
+    nestJsAdapter1.post(car1);
+    nestJsAdapter1.post(car2);
+    nestJsAdapter2.post(car3);
+    nestJsAdapter2.post(car4);
+    nestJsAdapter2.post(car5);
+
+    nestJsAdapter1.get();
+    nestJsAdapter1.getByType(RhinoCar);
+}
+
+function testExpressJsAdapter() {
+    const car6 = new MastodonCar('3','Mastodon Q','2024',125000000);
+
+    const expressJsFactory = new ExpressJsFactory();
+    const expressJsAdapter = expressJsFactory.useHttpAdapter(new MastodonCarServiceFactory());
+
+    expressJsAdapter.post(car6);
+    expressJsAdapter.get();
+}
+
+testNestJsAdapter();
+//testExpressJsAdapter();
