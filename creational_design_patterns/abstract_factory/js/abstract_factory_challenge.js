a8b5bafa35e1e0bf3b52c5d00f51254a9955fd61
andres hernandez <andrescodedev@gmail.com>
2024-11-29 16:24:40 -0500

    I finished the abstract factory design pattern challenge

--- creational_design_patterns/abstract_factory/js/abstract_factory_challenge.js
+++ creational_design_patterns/abstract_factory/js/abstract_factory_challenge.js
@@ -31,6 +31,7 @@ class CPUInterface {
     getCPUSerie() { throw new Error(`This method can't be implemented`) }
     getCPUApplication() { throw new Error(`This method can't be implemented`) }
     getCPUBrand() { throw new Error(`This method can't be implemented`) }
+    getCPUCoresNumber() { throw new Error(`This method can't be implemented`) }
 }
 
 class MemoryInterface {
@@ -54,23 +55,25 @@ class OperativeSystemInterface {
     getOperativeSystemBrand() { throw new Error(`This method can't be implemented`) }
 }
 
-class AssembledLaptopInterface {
-    getAssembledLaptopBrand() { throw new Error(`This method can't be implemented`) }
-    getAssembledLaptopCPU() { throw new Error(`This method can't be implemented`) }
-    getAssembledLaptopMemory() { throw new Error(`This method can't be implemented`) }
-    getAssembledLaptopDisplay() { throw new Error(`This method can't be implemented`) }
-    getAssembledLaptopOperativeSystem() { throw new Error(`This method can't be implemented`) }
-    getAssembledLaptopBasePrice() { throw new Error(`This method can't be implemented`) }
-    getAssembledLaptopSalePrice() { throw new Error(`This method can't be implemented`) }
+class AssembledDeviceInterface {
+    getAssembledDeviceBrand() { throw new Error(`This method can't be implemented`) }
+    getAssembledDeviceCPU() { throw new Error(`This method can't be implemented`) }
+    getAssembledDeviceMemory() { throw new Error(`This method can't be implemented`) }
+    getAssembledDeviceDisplay() { throw new Error(`This method can't be implemented`) }
+    getAssembledDeviceOperativeSystem() { throw new Error(`This method can't be implemented`) }
+    getAssembledDeviceBasePrice() { throw new Error(`This method can't be implemented`) }
+    getAssembledDeviceSalePrice() { throw new Error(`This method can't be implemented`) }
 }
 
 //ENTITIES - IMPLEMENTERS CLASSES
-class IntelCPU extends CPUInterface {
+class LaptopCPU extends CPUInterface {
 
-    constructor(serie, brand, application, price) {
+    constructor(serie, brand, coresNumber, price) {
+        super();
         this.serie = serie;
         this.brand = brand;
-        this.application = application;
+        this.coresNumber = coresNumber;
+        this.application = 'Laptop';
         this.price = price;
     }
 
@@ -82,6 +85,10 @@ class IntelCPU extends CPUInterface {
         return this.brand;
     }
 
+    getCPUCoresNumber() {
+        return this.coresNumber;
+    }
+
     getCPUApplication() {
         return this.application;
     }
@@ -91,11 +98,44 @@ class IntelCPU extends CPUInterface {
     }
 }
 
-class SODimmMemory extends MemoryInterface {
-    constructor(brand, capacityInGB, application, price) {
+class PhoneCPU extends CPUInterface {
+
+    constructor(serie, brand, coresNumber, price) {
+        super();
+        this.serie = serie;
+        this.brand = brand;
+        this.coresNumber = coresNumber;
+        this.application = 'Phone';
+        this.price = price;
+    }
+
+    getCPUSerie() {
+        return this.serie;
+    }
+
+    getCPUBrand() {
+        return this.brand;
+    }
+
+    getCPUCoresNumber() {
+        return this.coresNumber;
+    }
+
+    getCPUApplication() {
+        return this.application;
+    }
+
+    getCPUPrice() {
+        return this.price;
+    }
+}
+
+class LaptopSODimmMemory extends MemoryInterface {
+    constructor(brand, capacityInGB, price) {
+        super();
         this.brand = brand;
         this.capacityInGB = capacityInGB;
-        this.application = application;
+        this.application = 'Laptop';
         this.price = price;
     }
 
@@ -116,11 +156,38 @@ class SODimmMemory extends MemoryInterface {
     }
 }
 
-class LCDDisplay extends DisplayInterface {
-    constructor(brand, inches, application, price) {
+class PhoneSODimmMemory extends MemoryInterface {
+    constructor(brand, capacityInGB, price) {
+        super();
+        this.brand = brand;
+        this.capacityInGB = capacityInGB;
+        this.application = 'Phone';
+        this.price = price;
+    }
+
+    getMemoryBrand() {
+        return this.brand;
+    }
+
+    getMemoryCapacityInGb() {
+        return this.capacityInGB;
+    }
+
+    getMemoryApplication() {
+        return this.application;
+    }
+
+    getMemoryPrice() {
+        return this.price;
+    }
+}
+
+class LaptopLCDDisplay extends DisplayInterface {
+    constructor(brand, inches, price) {
+        super();
         this.brand = brand;
         this.inches = inches;
-        this.application = application;
+        this.application = 'Laptop';
         this.price = price;
     }
 
@@ -141,11 +208,38 @@ class LCDDisplay extends DisplayInterface {
     }
 }
 
-class WindowsOperativeSystem extends OperativeSystemInterface {
-    constructor(brand, version, application, price) {
+class PhoneLCDDisplay extends DisplayInterface {
+    constructor(brand, inches, price) {
+        super();
+        this.brand = brand;
+        this.inches = inches;
+        this.application = 'Phone';
+        this.price = price;
+    }
+
+    getDisplayBrand() {
+        return this.brand;
+    }
+
+    getDisplayInches() {
+        return this.inches;
+    }
+
+    getDisplayApplication() {
+        return this.application;
+    }
+
+    getDisplayPrice() {
+        return this.price;
+    }
+}
+
+class LaptopOperativeSystem extends OperativeSystemInterface {
+    constructor(brand, version, price) {
+        super();
         this.brand = brand;
         this.version = version;
-        this.application = application;
+        this.application = 'Laptop';
         this.price = price;
     }
 
@@ -166,47 +260,81 @@ class WindowsOperativeSystem extends OperativeSystemInterface {
     }
 }
 
-class AsusLaptop extends AssembledLaptopInterface {
-    constructor(brand, cpu, memory, display, operativeSystem, basePrice) {
+class PhoneOperativeSystem extends OperativeSystemInterface {
+    constructor(brand, version, price) {
+        super();
         this.brand = brand;
-        this.cpu = cpu;
-        this.memory = memory;
-        this.display = display;
-        this.operativeSystem = operativeSystem;
-        this.basePrice = basePrice;
-        this.salePrice = this.getAssembledLaptopSalePrice(new PriceCalculateServiceFactory());
+        this.version = version;
+        this.application = 'Phone';
+        this.price = price;
     }
 
-    getAssembledLaptopBrand() {
+    getOperativeSystemBrand() {
         return this.brand;
     }
 
-    getAssembledLaptopCPU() {
+    getOperativeSystemVersion() {
+        return this.version;
+    }
+
+    getOperativeSystemApplication() {
+        return this.application;
+    }
+
+    getOperativeSystemLicensePrice() {
+        return this.price;
+    }
+}
+
+class Laptop extends AssembledDeviceInterface {
+    constructor(
+        brand,
+        laptopCPU,
+        laptopMemory,
+        laptopDisplay,
+        laptopOperativeSystem,
+        basePrice
+    ) {
+        super();
+        this.brand = brand;
+        this.cpu = laptopCPU;
+        this.memory = laptopMemory;
+        this.display = laptopDisplay;
+        this.operativeSystem = laptopOperativeSystem;
+        this.basePrice = basePrice;
+        this.salePrice = this.getAssembledDeviceSalePrice(new PriceCalculateServiceFactory());
+    }
+
+    getAssembledDeviceBrand() {
+        return this.brand;
+    }
+
+    getAssembledDeviceCPU() {
         return this.cpu;
     }
 
-    getAssembledLaptopMemory() {
+    getAssembledDeviceMemory() {
         return this.memory;
     }
 
-    getAssembledLaptopDisplay() {
+    getAssembledDeviceDisplay() {
         return this.display;
     }
 
-    getAssembledLaptopOperativeSystem() {
+    getAssembledDeviceOperativeSystem() {
         return this.operativeSystem;
     }
 
-    getAssembledLaptopBasePrice() {
+    getAssembledDeviceBasePrice() {
         return this.basePrice;
     }
 
-    getAssembledLaptopSalePrice(priceCalculateServiceFactory) {
+    getAssembledDeviceSalePrice(priceCalculateServiceFactory) {
         let prices = [
-            this.cpu.price, 
-            this.memory.price, 
-            this.display.price, 
-            this.operativeSystem.price, 
+            this.cpu.price,
+            this.memory.price,
+            this.display.price,
+            this.operativeSystem.price,
             this.basePrice
         ];
 
@@ -215,9 +343,108 @@ class AsusLaptop extends AssembledLaptopInterface {
     }
 }
 
+class Phone extends AssembledDeviceInterface {
+    constructor(
+        brand,
+        phoneCPU,
+        phoneMemory,
+        phoneDisplay,
+        phoneOperativeSystem,
+        basePrice
+    ) {
+        super();
+        this.brand = brand;
+        this.cpu = phoneCPU;
+        this.memory = phoneMemory;
+        this.display = phoneDisplay;
+        this.operativeSystem = phoneOperativeSystem;
+        this.basePrice = basePrice;
+        this.salePrice = this.getAssembledDeviceSalePrice(new PriceCalculateServiceFactory());
+    }
+
+    getAssembledDeviceBrand() {
+        return this.brand;
+    }
+
+    getAssembledDeviceCPU() {
+        return this.cpu;
+    }
+
+    getAssembledDeviceMemory() {
+        return this.memory;
+    }
+
+    getAssembledDeviceDisplay() {
+        return this.display;
+    }
+
+    getAssembledDeviceOperativeSystem() {
+        return this.operativeSystem;
+    }
+
+    getAssembledDeviceBasePrice() {
+        return this.basePrice;
+    }
+
+    getAssembledDeviceSalePrice(priceCalculateServiceFactory) {
+        let prices = [
+            this.cpu.price,
+            this.memory.price,
+            this.display.price,
+            this.operativeSystem.price,
+            this.basePrice
+        ];
+
+        const priceCalculateService = priceCalculateServiceFactory.createPriceCalculateServiceInstance();
+        return priceCalculateService.calculatePhonePrice(prices);
+    }
+}
+
 // ENTITIES ABSTRACT FACTORY
+class LaptopAbstractFactory {
+    createLaptopCPUInstance() { throw new Error(`This method can't be implemented`) }
+    createLaptopSODimmMemoryInstance() { throw new Error(`This method can't be implemented`) }
+    createLaptopLCDDisplayInstance() { throw new Error(`This method can't be implemented`) }
+    createLaptopOperativeSystemInstance() { throw new Error(`This method can't be implemented`) }
+    createLaptopInstance() { throw new Error(`This method can't be implemented`) }
+}
+
+class PhoneAbstractFactory {
+    createPhoneCPUInstance() { throw new Error(`This method can't be implemented`) }
+    createPhoneSODimmMemoryInstance() { throw new Error(`This method can't be implemented`) }
+    createPhoneLCDDisplayInstance() { throw new Error(`This method can't be implemented`) }
+    createPhoneOperativeSystemInstance() { throw new Error(`This method can't be implemented`) }
+    createPhoneInstance() { throw new Error(`This method can't be implemented`) }
+}
 
 // ENTITIES FACTORIES
+class LaptopFactory extends LaptopAbstractFactory {
+    createLaptopCPUInstance(serie, brand, coresNumber, price) {
+        return new LaptopCPU(serie, brand, coresNumber, price);
+    }
+
+    createLaptopSODimmMemoryInstance(brand, capacityInGB, price) {
+        return new LaptopSODimmMemory(brand, capacityInGB, price);
+    }
+
+    createLaptopLCDDisplayInstance(brand, inches, price) {
+        return new LaptopLCDDisplay(brand, inches, price);
+    }
+
+    createLaptopOperativeSystemInstance(brand, version, price) {
+        return new LaptopOperativeSystem(brand, version, price);
+    }
+
+    createLaptopInstance(brand, laptopCPU, laptopMemory, laptopDisplay, laptopOperativeSystem, basePrice) {
+        return new Laptop(brand, laptopCPU, laptopMemory, laptopDisplay, laptopOperativeSystem, basePrice);
+    }
+}
+
+/*class PhoneFactory extends PhoneAbstractFactory {
+    createGeneralCPUInstance(serie, brand, coresNumber, application, price) {
+        return new GeneralCPU(serie, brand, coresNumber, application, price);
+    }
+}*/
 
 //SERVICES INTERFACES
 
@@ -247,6 +474,24 @@ class PriceCalculateServiceFactory {
         return new PriceCalculateService();
     }
 }
+
+
+
+// APP TESTING
+function testApp() {
+    const laptopFactory = new LaptopFactory();
+    const cpu = laptopFactory.createLaptopCPUInstance('core i5','intel',2,120000);
+    const memory = laptopFactory.createLaptopSODimmMemoryInstance('GEFORCE','8',75000);
+    const display = laptopFactory.createLaptopLCDDisplayInstance('lcdp','21',95000);
+    const operativeSystem = laptopFactory.createLaptopOperativeSystemInstance('Microsoft','windows 10',135000);
+
+    const laptop = laptopFactory.createLaptopInstance('ASUS',cpu,memory,display,operativeSystem,1200000);
+
+    console.log(laptop);
+   
+}
+
+testApp();
 
 
 
