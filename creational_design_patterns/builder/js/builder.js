11d8ce6f2bac29bdc7437d8bfbb32d5b3e8f9a2c
andres hernandez <andrescodedev@gmail.com>
2024-12-03 21:56:21 -0500

    I'm working on the builder design pattern challenge

--- creational_design_patterns/builder/js/builder.js
+++ creational_design_patterns/builder/js/builder.js
@@ -2,7 +2,44 @@
     BUILDER DESIGN PATTERN
 
     El patrón de diseño Builder, nos permite dividir la creación de un objetos en pasos. Utilizando el mismo
-    proceso de construción, podemos crear diferentes representaciones del mismo objeto.
+    proceso de construción, podemos crear diferentes tipos y representaciones de un objeto.
+
+    What does it problem solve?
+        1. Problemas de reusablidad.
+        2. Problemas de extensibilidad.
+        3. Problemas de fuerte acoplamiento.
+        4. Problemas de constructor microscópico
+        5. Problemas de objeto complejo
+        6. Problemas de herencia compleja
+
+    How does it implement?
+        1. Crear una clase base / interfaz que define los pasos de construcción de producto que
+           todos los tipos de objetos constructores tienen en común.
+        2. Los constructores concretos ofrecen distintas implementaciones de los pasos de construcción.
+           Los constructores concretos pueden crear productos que no siguen la interfaz común.
+        3. Los productos son los objetos resultantes. Los productos construidos por distintos objetos
+           constructores, no tienen que pertenecer a la misma jerarquía de clase o interfaz.
+        4. La clase directora define el orden en el que se invocaran los pasos de construcción, por
+           lo que puedes crear y reutilizar configuraciones específicas de los productos.
+        5. 
+
+    PROS
+        - Construir objetos paso a paso.
+        - Reutilizar el mismo proceso de construcción, para construir diferentes representaciones
+          de productos.
+        - Aislar las configuraciones de construcción en un solo lugar.
+        - Nuevas configuraciones no requieren modificar las existentes.
+
+    AGAINT
+        - Demasiado código genérico, nuevo producto -> nuevo builder.
+        - Mutación del objeto producto.
+
+    When use it?
+        - Cuando quieras evitar tener un constructos telescópico.
+        - Cuando requieras que existan diferentes representaciones de algunos productos.
+        - Cuando quieras tener control sobre el proceso de creación de un objeto.
+
+
 */
 
 // BASE CLASS OR INTERFACE
@@ -125,7 +162,7 @@ class Car {
     set gps(gps) {
         this._gps = gps;
     }
-    
+
 }
 
 class CarManual {
@@ -159,22 +196,31 @@ class CarManual {
 }
 
 class Director {
-    
+
     buildCar(builder) {
         builder.reset();
-        builder.seatsNumber(3);
+        builder.setSeats(3);
         builder.setEngine('Sports Engine');
         builder.setGPS('GPS Location');
     }
 
     buildCarManual(builder) {
         builder.reset();
-        builder.seatsNumber(3);
-        builder.setEngine('Sports Engine');
-        builder.setGPS('GPS Location');
+        builder.setSeats(3)
+               .setEngine('Sports Engine')
+               .setGPS('GPS Location');
     }
 }
 
-/*class testApp() {
+function testApp() {
+    const director = new Director();
+    const carBuilder = new CarBuilder();
 
-}*/
+    director.buildCar(carBuilder);
+    const car = carBuilder.getCar();
+
+    console.log(car);
+}
+
+testApp();
+
---
+++ creational_design_patterns/builder/js/builder_challenge.js
@@ -0,0 +1,30 @@
+/* 
+    BUILDER CHALLENGE
+*/
+
+// USE CASE PROBLEM
+// HERE, WE HAVE THE COMPLEX OBJECT PROBLEM
+class Laptop {
+
+    // HERE, WE HAVE THE MICROSCOPIC CONSTRUCTOR PROBLEM
+    constructor(cpu, memory, hardDisk, keyboard, screen, camera, microphone, battery, usbPorts, tactileMouse, hdmiPorts, earphonePort, sdPort, graphicCard, operativeSystem) {
+
+
+    }
+}
+
+// THIS IS COMPLEX OBJECT PROBLEM
+class Phone {
+    // HERE, WE HAVE THE MICROSCOPIC CONSTRUCTOR PROBLEM
+    constructor(cpu, memory, screen, camera, microphone, battery, earphonePort, gps, sensors, operativeSystem) {
+
+    }
+}
+
+// THIS IS COMPLEX OBJECT PROBLEM
+class Tablet {
+    // HERE, WE HAVE THE MICROSCOPIC CONSTRUCTOR PROBLEM
+    constructor(cpu, memory, screen, camera, microphone, battery, earphonePort, gps, sensors, operativeSystem) {
+
+    }
+}
