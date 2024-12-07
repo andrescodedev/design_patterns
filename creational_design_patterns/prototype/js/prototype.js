7bee9553805f80c8f46d60254d1e333c9fac8836
andres hernandez <andrescodedev@gmail.com>
2024-12-06 21:24:22 -0500

    I continue with prototype design pattern

--- creational_design_patterns/prototype/js/prototype.js
+++ creational_design_patterns/prototype/js/prototype.js
@@ -1,5 +1,32 @@
 /* 
     PROTOTYPE DESIGN PATTERN
 
-    
+    Nos permite copiar objetos existentes sin que el código dependa de sus clases
+
+
 */
+
+class PrototypeInterface {
+
+    createClone() {throw new Error(`This method can't be implemented`)}
+}
+
+class Airplane extends PrototypeInterface {
+
+    createClone() {
+        console.log(`Creating clone`);
+    }
+
+    createClone(flightNumber) {
+        console.log(`The flight number is ${flightNumber}`);
+    }
+    
+}
+
+function testApp() {
+    const airplane = new Airplane();
+
+    airplane.createClone();
+}
+
+testApp();
